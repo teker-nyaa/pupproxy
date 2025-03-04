7cf09225baf9e31aacda386623064b8cdb6804aa
Teker <ellieputnam5@gmail.com>
2025-03-04 15:25:37 +0000

    arf :3

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<div id="header">
+	<h1 class="text-2xl font-medium text-rose-300">PupProxy</h1>
+</div>
+
+{@render children()}
